39abf4be52a90fb94b9eb12fd1b21ba2f67347c9
Marc <>
2018-03-16 17:23:25 +0100

    style(visitors): remove debugging statements

--- src/src/mmc/modal/visitors/EmersonLeiAlgorithm.java
+++ src/src/mmc/modal/visitors/EmersonLeiAlgorithm.java
@@ -27,20 +27,12 @@ public class EmersonLeiAlgorithm extends NaiveAlgorithm implements FormulaCalcul
         RecursionVariable recursionVariable = formula.getRecursionVariable();
         Formula subFormula = formula.getFormula();
 
-        System.out.println("*** free bounded all ***");
-        System.out.println(formula.getVariableMatcher().getFree());
-        System.out.println(formula.getVariableMatcher().getBounded());
-        System.out.println(formula.getVariableMatcher().getAll());
-        System.out.println("*** ***");
-
         if(!this.getFixedPointResults().containsKey(recursionVariable)) {
-            System.out.println(String.format("mu init %s", recursionVariable));
             this.getFixedPointResults().put(recursionVariable, new HashSet<>());
         }
 
         if (this.bound == Bound.NU_BOUNDED) {
             for (RecursionVariable boundedVariable: formula.getVariableMatcher().getBounded()) {
-                System.out.println(String.format("mu reset %s", boundedVariable));
                 this.getFixedPointResults().put(boundedVariable, new HashSet<>());
             }
         }
@@ -56,20 +48,12 @@ public class EmersonLeiAlgorithm extends NaiveAlgorithm implements FormulaCalcul
         RecursionVariable recursionVariable = formula.getRecursionVariable();
         Formula subFormula = formula.getFormula();
 
-        System.out.println("*** free bounded all ***");
-        System.out.println(formula.getVariableMatcher().getFree());
-        System.out.println(formula.getVariableMatcher().getBounded());
-        System.out.println(formula.getVariableMatcher().getAll());
-        System.out.println("*** ***");
-
         if(!this.getFixedPointResults().containsKey(recursionVariable)) {
-            System.out.println(String.format("nu init %s", recursionVariable));
             this.getFixedPointResults().put(recursionVariable, this.getStates());
         }
 
         if (this.bound == Bound.MU_BOUNDED) {
             for (RecursionVariable boundedVariable: formula.getVariableMatcher().getBounded()) {
-                System.out.println(String.format("nu reset %s", boundedVariable));
                 this.getFixedPointResults().put(boundedVariable, this.getStates());
             }
         }
--- src/src/mmc/modal/visitors/VariableMatcher.java
+++ src/src/mmc/modal/visitors/VariableMatcher.java
@@ -74,7 +74,6 @@ public class VariableMatcher extends RecursiveVisitor implements FormulaVisitor 
 
     @Override
     public void visit(RecursionVariable formula) {
-        System.out.println("            " + formula.toString());
         this.all.add(formula);
         if(this.variable.equals(formula)) {
             this.bounded.add(formula);
